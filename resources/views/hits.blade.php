e89e878b02e0f607d74c99c2a4c62714a00a21e2
Chris Morrell <inxilpro@users.noreply.github.com>
2021-01-25 14:04:20 -0500

    Empty slot

--- resources/views/hits.blade.php
+++ resources/views/hits.blade.php
@@ -4,4 +4,10 @@
 	<template x-for="hit in hits" :key="hit.objectID">
 		{{ $slot }}
 	</template>
+	
+	@isset($empty)
+		<div style="display: none;" x-show="!first_render && 0 === hits.length">
+			{{ $empty }}
+		</div>
+	@endisset
 </div>
