07f05330f028dae9507b5c1feafdec5b5f1a43f9
Skyler Katz <skylerkatz@hey.com>
2023-04-18 12:40:32 -0500

    add clear-refinements component
    Signed-off-by: Skyler Katz <skylerkatz@hey.com>

--- resources/views/clear-refinements.blade.php
+++ resources/views/clear-refinements.blade.php
@@ -1,9 +1,11 @@
 <?php /** @var \Illuminate\View\ComponentAttributeBag $attributes */ ?>
 
-<div {{ $attributes }}>
-	{{ $slot }}
-</div>
-
-{{-- includedAttributes (?array) --}}
-{{-- excludedAttributes (?array) --}}
-{{-- id (?string) --}}
+<button
+	{{ $attributes->merge([
+		'class'=> 'block w-full px-2 py-1 rounded border border-gray-300 bg-white text-sm font-medium hover:bg-gray-100 text-gray-500 cursor-pointer'
+	]) }}
+	x-show.important="canRefine"
+	@click="refine()"
+>
+	{{ $slot->isNotEmpty() ? $slot : 'Clear Refinements' }}
+</button>
